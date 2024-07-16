be58949c3665163746a205941c83dbb6c23165ae
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-07-16 21:50:12 +0800

    trying a function to work with session

--- core/Session.php
+++ core/Session.php
@@ -8,6 +8,8 @@ class Session
     public function __construct()
     {
         session_start();
+        $flashMessages = $_SESSION[self::FLASH_KEY];
+        echo "IM THE CONSTRUCTOR " . var_dump($flashMessages) ;
     }
 
     public static function set_flash(string $key, string $value): void
@@ -18,5 +20,9 @@ class Session
     public static function get_flash(string $key): string|bool
     {
         return $_SESSION[self::FLASH_KEY][$key] ?? false;
+    }
+    public function __destruct()
+    {
+        echo  "DECONSTRUCTOR " . var_dump($_SESSION[self::FLASH_KEY]);
     }
 }
